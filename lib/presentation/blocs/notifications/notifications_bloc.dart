0e697a3f9bbd919f400a584269813b1481226a3d
ivgomez <iv.gomez@hotmail.com>
2025-03-02 10:03:31 -0500

    first commit

---
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+part 'notifications_event.dart';
+part 'notifications_state.dart';
+
+class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationsBloc() : super( const NotificationsState() ) {
+    // on<NotificationsEvent>((event, emit) {
+    //   // TODO: implement event handler
+    // });
+  }
+
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    settings.authorizationStatus;
+  }
+}
