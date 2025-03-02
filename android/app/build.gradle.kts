93c0bb64552cc4a0513a6038870524ebd3706281
ivgomez <iv.gomez@hotmail.com>
2025-03-02 10:04:44 -0500

    renaming app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.push_app"
+    namespace = "com.idgarcia.push_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.push_app"
+        applicationId = "com.idgarcia.push_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = "27.0.12077973"
